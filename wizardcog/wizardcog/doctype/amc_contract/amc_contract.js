3691f5d2e50a787ad0097e2ee4fac769de587873
priyatoshy <priyatoshsatpati1999@gmail.com>
2024-09-05 01:32:14 +0530

    website change

---
+++ wizardcog/wizardcog/doctype/amc_contract/amc_contract.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, WizardCog Technologies and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("AMC Contract", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ wizardcog/wizardcog/doctype/amc_contract/test_amc_contract.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, WizardCog Technologies and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAMCContract(FrappeTestCase):
+	pass
